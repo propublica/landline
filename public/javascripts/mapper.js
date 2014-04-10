6de7da1eebfffbc1b1cc360ec3e5470b098c5bed
Al Shaw <almshaw@gmail.com>
2014-04-10 13:18:42 -0700

    first stab at a MapperCanvas class

--- public/javascripts/mapper.js
+++ public/javascripts/mapper.js
@@ -1,4 +1,3 @@
-
 (function(){
   var Map = this.Mapper = function(data){
     this.data = data;
@@ -291,6 +290,50 @@
 
   FeatureCollection.prototype.project = function(proj){
     this._project = proj;
+  };
+
+  // MapCanvas puts default collections of maps together for you
+  // Requires jQuery and Raphael.
+  var MapCanvas = this.MapperCanvas = function(container, locality, lookup) {
+    this.paper     = {};
+    this.locality  = locality;
+    this.container = $(container);
+    this.lookup    = (lookup || {});
+    this.setupHtml();
+    this.createMap(locality);
+  };
+
+  MapCanvas.DEFAULTS = [
+    {name : "continental", width : 950, height : 550, el : "mappercanvas_continental", 'z-index' : "999"},
+    {name : "alaska",      width : 250, height : 150, el : "mappercanvas_alaska",      'z-index' : "999"},
+    {name : "hawaii",      width : 150, height : 120, el : "mappercanvas_hawaii",      'z-index' : "999"}
+  ];
+
+  MapCanvas.prototype.setupHtml = function() {
+    var defaults = MapCanvas.DEFAULTS;
+    for (i = 0; i < defaults.length; i++) {
+      var regionEl = this.container.append("<div id='" + defaults[i].el + "'></div>");
+      this.paper[defaults[i].name] = Raphael(regionEl, defaults[i].width, defaults[i].height);
+    }
+  };
+
+  MapCanvas.prototype.createMap = function(locality) {
+    var data;
+    var that     = this;
+    var defaults = MapCanvas.DEFAULTS;
+    if (locality === "states")   data = window.MapperStates;
+    if (locality === "counties") data = window.MapperCounties;
+    for (i = 0; i < defaults.length; i++) {
+      // console.log(data[defaults[i].name]);
+      var localityMap = new Mapper(data[defaults[i].name]).all();
+      localityMap.asSVG(defaults[i].width, defaults[i].height, function(svg, it) {
+        var path = that.paper[defaults[i].name].path(svg);
+          path.attr("fill", "#b93737")
+              .attr('stroke-width', 0.5)
+              .attr('stroke', '#fff')
+              .attr('stroke-linejoin', 'bevel');
+      });
+    }
   };
 
 }).call(this);
